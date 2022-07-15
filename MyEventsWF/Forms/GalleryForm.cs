cf6df7add23512f35bc19e51d0412f49b81789eb
Drudkh <39151995+TomkaYurii@users.noreply.github.com>
2022-07-16 02:00:05 +0300

    Added new functionality (added menu)

---
+++ MyEventsWF/Forms/GalleryForm.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyEventsWF.Forms
+{
+    public partial class GalleryForm : Form
+    {
+        public GalleryForm()
+        {
+            InitializeComponent();
+            LoadTheme();
+        }
+        private void LoadTheme()
+        {
+            foreach (Control btns in this.Controls)
+            {
+                if (btns.GetType() == typeof(Button))
+                {
+                    Button btn = (Button)btns;
+                    btn.BackColor = ThemeColor.PrimaryColor;
+                    btn.ForeColor = Color.White;
+                    btn.FlatAppearance.BorderColor = ThemeColor.SecondaryColor;
+                }
+            }
+            label1.ForeColor = ThemeColor.SecondaryColor;
+            label2.ForeColor = ThemeColor.SecondaryColor;
+            label3.ForeColor = ThemeColor.SecondaryColor;
+            label4.ForeColor = ThemeColor.SecondaryColor;
+            label5.ForeColor = ThemeColor.SecondaryColor;
+            label6.ForeColor = ThemeColor.PrimaryColor;
+            label7.ForeColor = ThemeColor.PrimaryColor;
+            label8.ForeColor = ThemeColor.PrimaryColor;
+            label9.ForeColor = ThemeColor.PrimaryColor;
+            label10.ForeColor = ThemeColor.PrimaryColor;
+        }
+    }
+}
